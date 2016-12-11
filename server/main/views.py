5548b9f96720b1a75da792fd8d44904a00553a4e
Vipin Chaudhary <vipin14119@iiitd.ac.in>
2016-12-11 13:06:33 +0530

    changed some text and added links in header

--- server/main/views.py
+++ server/main/views.py
@@ -4,32 +4,32 @@ from main.forms import ImageUploadForm
 from django.contrib import messages
 
 
-# def index(request):
-#     form = ImageUploadForm(request.POST, request.FILES or None)
-#     if request.method == 'POST':
-#         if form.is_valid():
-#             messages.add_message(request, messages.SUCCESS,"Form uploaded successfully")
-#             files = request.FILES.getlist('images')
-#             for file in files:
-#                 print file
-#             return render(request, 'main/index.html', {
-#                 'title': 'Hypothizer Labs',
-#                 'form': form
-#             })
-#         else:
-#             messages.add_message(request, messages.WARNING, "Please select a file")
-#             print "Handle invalid post"
-#             return render(request, 'main/index.html', {
-#                 'title': 'Hypothizer Labs',
-#                 'form': form
-#             })
-#     else:
-#         print "handle get"
-#         return render(request, 'main/index.html', {
-#             'title': 'Hypothizer Labs',
-#             'form': form
-#         })
-#
+def demo(request):
+    form = ImageUploadForm(request.POST, request.FILES or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            messages.add_message(request, messages.SUCCESS,"Form uploaded successfully")
+            files = request.FILES.getlist('images')
+            for file in files:
+                print file
+            return render(request, 'demo.html', {
+                'title': 'Hypothizer Labs',
+                'form': form
+            })
+        else:
+            messages.add_message(request, messages.WARNING, "Please select a file")
+            print "Handle invalid post"
+            return render(request, 'demo.html', {
+                'title': 'Hypothizer Labs',
+                'form': form
+            })
+    else:
+        print "handle get"
+        return render(request, 'demo.html', {
+            'title': 'Hypothizer Labs',
+            'form': form
+        })
+
 
 def index(request):
     return render(request, 'index_grey.html')
@@ -43,7 +43,5 @@ def signup(request):
     return render(request, 'signup_login.html')
 
 
-def demo(request):
-    return render(request, 'demo.html')
 
 
