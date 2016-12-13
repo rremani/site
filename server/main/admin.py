e3cdde1ecae7331001e9a147ebe5614c539dd89c
Vipin Chaudhary <vipin14119@iiitd.ac.in>
2016-12-13 21:26:42 +0530

    password field added for user login and signup

--- server/main/admin.py
+++ server/main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from main.models import HypoUser
 
-# Register your models here.
+
+class AdminHypoUser(admin.ModelAdmin):
+    list_display = ['username', 'email', 'name']
+
+    class Meta:
+        model = HypoUser
+
+admin.site.register(HypoUser, AdminHypoUser)
--- server/main/models.py
+++ server/main/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class HypoUser(models.Model):
+    username = models.CharField(max_length=25, null=False, blank=False, unique=True)
+    password = models.CharField(max_length=25, null=False, blank=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=50, null=False, blank=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
