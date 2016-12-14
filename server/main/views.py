7a70cf8765651c75220b511170c3d5ff1ddd1ef9
Vipin Chaudhary <vipin14119@iiitd.ac.in>
2016-12-14 23:43:44 +0530

    method position corrected in view.py

--- server/main/views.py
+++ server/main/views.py
@@ -6,34 +6,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, get_backends
 
 
-def demo(request):
-    print(request.user)
-    form = ImageUploadForm(request.POST, request.FILES or None)
-    if request.method == 'POST':
-        if form.is_valid():
-            messages.add_message(request, messages.SUCCESS,"Form uploaded successfully")
-            files = request.FILES.getlist('images')
-            for file in files:
-                print file
-            return render(request, 'demo.html', {
-                'title': 'Hypothizer Labs',
-                'form': form
-            })
-        else:
-            messages.add_message(request, messages.WARNING, "Please select a file")
-            print "Handle invalid post"
-            return render(request, 'demo.html', {
-                'title': 'Hypothizer Labs',
-                'form': form
-            })
-    else:
-        print "handle get"
-        return render(request, 'demo.html', {
-            'title': 'Hypothizer Labs',
-            'form': form
-        })
-
-
 def index(request):
     return render(request, 'index_grey.html')
 
@@ -100,5 +72,29 @@ def login_signup(request):
         return render(request, 'signup_login.html', {'login_form': login_form, 'signup_form': signup_form})
 
 
-
-
+def demo(request):
+    print(request.user)
+    form = ImageUploadForm(request.POST, request.FILES or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            messages.add_message(request, messages.SUCCESS,"Form uploaded successfully")
+            files = request.FILES.getlist('images')
+            for file in files:
+                print file
+            return render(request, 'demo.html', {
+                'title': 'Hypothizer Labs',
+                'form': form
+            })
+        else:
+            messages.add_message(request, messages.WARNING, "Please select a file")
+            print "Handle invalid post"
+            return render(request, 'demo.html', {
+                'title': 'Hypothizer Labs',
+                'form': form
+            })
+    else:
+        print "handle get"
+        return render(request, 'demo.html', {
+            'title': 'Hypothizer Labs',
+            'form': form
+        })
