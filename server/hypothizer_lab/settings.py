8f9cdba9c5c6422177348bc944227ceeed27f9cc
Vipin Chaudhary <vipin14119@iiitd.ac.in>
2016-12-16 20:13:27 +0530

    setting up social auth library

--- server/hypothizer_lab/settings.py
+++ server/hypothizer_lab/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'social.apps.django_app.default',
 ]
 
 MIDDLEWARE_CLASSES = [
